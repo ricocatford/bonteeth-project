31c9aa92e703aa6402a5672a0c295e15a2a6cf93
ricocatford <s7funoob@gmail.com>
2023-02-14 22:26:52 +0000

    Successfully connected Mongo DB to Flask and added templates directory with test html page for rendering appointments from DB

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
 import os
-from flask import Flask
+from flask import Flask, flash, render_template, redirect, request, session, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
 
 
 @app.route("/")
-def home():
-    return "Hello mate!"
+@app.route("/get_appointments")
+def get_appointments():
+    appointments = mongo.db.appointments.find()
+    return render_template("base.html", appointments=appointments)
 
 
 if __name__ == "__main__":
