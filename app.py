8d5538cfad58dab2f3771e8b19863e6ea66c7080
ricocatford <s7funoob@gmail.com>
2023-02-15 15:21:17 +0000

    Add registration fully functional and Login page

--- app.py
+++ app.py
@@ -23,7 +23,33 @@ def home():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+        existing_email = mongo.db.users.find_one(
+            {"email_address": request.form.get("email_address").lower()})
+
+        if existing_user or existing_email:
+            flash(f"Username and/or email address already exist.")
+            return redirect(url_for("register"))
+
+        register = {
+            "email_address": request.form.get("email_address").lower(),
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+
+        mongo.db.users.insert_one(register)
+
+        session["user"] = request.form.get("username").lower()
+        flash("Registration successful")
+
     return render_template("register.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html", methods=["GET", "POST"])
 
 
 @app.route("/get_appointments")
