1124fc9aeb0205823b6279c676dd0a038053ead9
ricocatford <s7funoob@gmail.com>
2023-02-23 12:33:15 +0000

    Switch debug mode off

--- app.py
+++ app.py
@@ -269,4 +269,4 @@ def delete_appointment(appointment_id):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
